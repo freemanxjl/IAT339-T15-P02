3929be216a36811f346f48d5ec15c94cb8da02bb
EJ Nicolas <ejnicolas001@gmail.com>
2021-03-17 19:24:42 -0700

    Fixed errors on nav file
    Added if statements to prevent the nullpointerexceptions which were happening

--- js/nav.js
+++ js/nav.js
@@ -21,14 +21,17 @@ if(width < 1100){
   buttonHamburger.setAttribute("aria-hidden", "false");
   navDesktop.classList.add("hidden");
   navDesktop.setAttribute("aria-hidden", "true");
-  navMobileStyleGuide.classList.remove("hidden");
-  navMobileStyleGuide.classList.add("mobile-navbar");
-  navMobileStyleGuide.setAttribute("aria-hidden", "false");
-  buttonHamburgerStyleGuide.classList.remove("hidden");
-  buttonHamburgerStyleGuide.classList.add("hamburger-button")
-  buttonHamburgerStyleGuide.setAttribute("aria-hidden", "false");
-  navDesktopStyleGuide.classList.add("hidden");
-  navDesktopStyleGuide.setAttribute("aria-hidden", "true");
+  if(!navDesktopStyleGuide == null){
+    navMobileStyleGuide.classList.remove("hidden");
+    navMobileStyleGuide.classList.add("mobile-navbar");
+    navMobileStyleGuide.setAttribute("aria-hidden", "false");
+    buttonHamburgerStyleGuide.classList.remove("hidden");
+    buttonHamburgerStyleGuide.classList.add("hamburger-button");
+    buttonHamburgerStyleGuide.setAttribute("aria-hidden", "false");
+    navDesktopStyleGuide.classList.add("hidden");
+    navDesktopStyleGuide.setAttribute("aria-hidden", "true");
+  }
+
 }
 else{
   navMobile.classList.add("hidden");
@@ -39,14 +42,16 @@ else{
   buttonHamburger.setAttribute("aria-hidden", "true");
   navDesktop.classList.remove("hidden");
   navDesktop.setAttribute("aria-hidden", "false");
-  navMobileStyleGuide.classList.add("hidden");
-  navMobileStyleGuide.classList.remove("mobile-navbar");
-  navMobileStyleGuide.setAttribute("aria-hidden", "true");
-  buttonHamburgerStyleGuide.classList.add("hidden");
-  buttonHamburgerStyleGuide.classList.remove("hamburger-button")
-  buttonHamburgerStyleGuide.setAttribute("aria-hidden", "true");
-  navDesktopStyleGuide.classList.remove("hidden");
-  navDesktopStyleGuide.setAttribute("aria-hidden", "false");
+  if(!navDesktopStyleGuide==null){
+    navMobileStyleGuide.classList.add("hidden");
+    navMobileStyleGuide.classList.remove("mobile-navbar");
+    navMobileStyleGuide.setAttribute("aria-hidden", "true");
+    buttonHamburgerStyleGuide.classList.add("hidden");
+    buttonHamburgerStyleGuide.classList.remove("hamburger-button");
+    buttonHamburgerStyleGuide.setAttribute("aria-hidden", "true");
+    navDesktopStyleGuide.classList.remove("hidden");
+    navDesktopStyleGuide.setAttribute("aria-hidden", "false");
+  }
 }
 
 window.addEventListener("resize", function(){
@@ -60,14 +65,16 @@ window.addEventListener("resize", function(){
     buttonHamburger.setAttribute("aria-hidden", "false");
     navDesktop.classList.add("hidden");
     navDesktop.setAttribute("aria-hidden", "true");
-    navMobileStyleGuide.classList.remove("hidden");
-    navMobileStyleGuide.classList.add("mobile-navbar");
-    navMobileStyleGuide.setAttribute("aria-hidden", "false");
-    buttonHamburgerStyleGuide.classList.remove("hidden");
-    buttonHamburgerStyleGuide.classList.add("hamburger-button")
-    buttonHamburgerStyleGuide.setAttribute("aria-hidden", "false");
-    navDesktopStyleGuide.classList.add("hidden");
-    navDesktopStyleGuide.setAttribute("aria-hidden", "true");
+    if(!navDesktopStyleGuide == null){
+      navMobileStyleGuide.classList.remove("hidden");
+      navMobileStyleGuide.classList.add("mobile-navbar");
+      navMobileStyleGuide.setAttribute("aria-hidden", "false");
+      buttonHamburgerStyleGuide.classList.remove("hidden");
+      buttonHamburgerStyleGuide.classList.add("hamburger-button");
+      buttonHamburgerStyleGuide.setAttribute("aria-hidden", "false");
+      navDesktopStyleGuide.classList.add("hidden");
+      navDesktopStyleGuide.setAttribute("aria-hidden", "true");
+    }
   }
   else{
     navMobile.classList.add("hidden");
@@ -78,14 +85,16 @@ window.addEventListener("resize", function(){
     buttonHamburger.setAttribute("aria-hidden", "true");
     navDesktop.classList.remove("hidden");
     navDesktop.setAttribute("aria-hidden", "false");
-    navMobileStyleGuide.classList.add("hidden");
-    navMobileStyleGuide.classList.remove("mobile-navbar");
-    navMobileStyleGuide.setAttribute("aria-hidden", "true");
-    buttonHamburgerStyleGuide.classList.add("hidden");
-    buttonHamburgerStyleGuide.classList.remove("hamburger-button")
-    buttonHamburgerStyleGuide.setAttribute("aria-hidden", "true");
-    navDesktopStyleGuide.classList.remove("hidden");
-    navDesktopStyleGuide.setAttribute("aria-hidden", "false");
+    if(!navDesktopStyleGuide == null){
+      navMobileStyleGuide.classList.add("hidden");
+      navMobileStyleGuide.classList.remove("mobile-navbar");
+      navMobileStyleGuide.setAttribute("aria-hidden", "true");
+      buttonHamburgerStyleGuide.classList.add("hidden");
+      buttonHamburgerStyleGuide.classList.remove("hamburger-button");
+      buttonHamburgerStyleGuide.setAttribute("aria-hidden", "true");
+      navDesktopStyleGuide.classList.remove("hidden");
+      navDesktopStyleGuide.setAttribute("aria-hidden", "false");
+    }
   }
 });
 
@@ -102,18 +111,21 @@ buttonHamburger.addEventListener("click", function(){
   }
 });
 
-buttonHamburgerStyleGuide.addEventListener("click", function(){
-  if(mobileNavItemsStyleGuide.classList.contains("hidden")){
-    mobileNavItemsStyleGuide.classList.remove("hidden");
-    mobileNavItemsStyleGuide.classList.add("dropdown-wrapper");
-    mobileNavItemsStyleGuide.setAttribute("aria-hidden", "false");
-  }
-  else{
-    mobileNavItemsStyleGuide.classList.add("hidden");
-    mobileNavItemsStyleGuide.classList.remove("dropdown-wrapper");
-    mobileNavItemsStyleGuide.setAttribute("aria-hidden", "true");
-  }
-});
+if(!buttonHamburgerStyleGuide == null){
+  buttonHamburgerStyleGuide.addEventListener("click", function(){
+    if(mobileNavItemsStyleGuide.classList.contains("hidden")){
+      mobileNavItemsStyleGuide.classList.remove("hidden");
+      mobileNavItemsStyleGuide.classList.add("dropdown-wrapper");
+      mobileNavItemsStyleGuide.setAttribute("aria-hidden", "false");
+    }
+    else{
+      mobileNavItemsStyleGuide.classList.add("hidden");
+      mobileNavItemsStyleGuide.classList.remove("dropdown-wrapper");
+      mobileNavItemsStyleGuide.setAttribute("aria-hidden", "true");
+    }
+  });
+}
+
 
 //keypress event listener referred from https://stackoverflow.com/questions/14542062/eventlistener-enter-key/50993410
 buttonHamburger.addEventListener("keypress", function(e){
@@ -131,17 +143,19 @@ buttonHamburger.addEventListener("keypress", function(e){
   }
 });
 
-buttonHamburgerStyleGuide.addEventListener("keypress", function(e){
-  if(e.key == 'Enter'){
-    if(mobileNavItemsStyleGuide.classList.contains("hidden")){
-      mobileNavItemsStyleGuide.classList.remove("hidden");
-      mobileNavItemsStyleGuide.classList.add("dropdown-wrapper");
-      mobileNavItemsStyleGuide.setAttribute("aria-hidden", "false");
+if(!buttonHamburgerStyleGuide==null){
+  buttonHamburgerStyleGuide.addEventListener("keypress", function(e){
+    if(e.key == 'Enter'){
+      if(mobileNavItemsStyleGuide.classList.contains("hidden")){
+        mobileNavItemsStyleGuide.classList.remove("hidden");
+        mobileNavItemsStyleGuide.classList.add("dropdown-wrapper");
+        mobileNavItemsStyleGuide.setAttribute("aria-hidden", "false");
+      }
+      else{
+        mobileNavItemsStyleGuide.classList.add("hidden");
+        mobileNavItemsStyleGuide.classList.remove("dropdown-wrapper");
+        mobileNavItemsStyleGuide.setAttribute("aria-hidden", "true");
+      }
     }
-    else{
-      mobileNavItemsStyleGuide.classList.add("hidden");
-      mobileNavItemsStyleGuide.classList.remove("dropdown-wrapper");
-      mobileNavItemsStyleGuide.setAttribute("aria-hidden", "true");
-    }
-  }
-});
+  });
+}
